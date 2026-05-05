990551d481c36901be73169239e1fc8350a79d61
Felix Lehmann <buffygr@web.de>
2026-05-05 22:13:06 +0200

    some cool lights stuff

---
+++ Lighting/LightEffect.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class LightEffect : PointLight2D
+{
+	[Export] public float Speed = 3f;
+	[Export] public float EnergyVariation = 0.015f;
+	[Export] public float SizeVariation = 0.015f;
+
+	private float _time;
+	private float _baseEnergy;
+	private float _baseScale;
+	private float _offset;
+
+	public override void _Ready()
+	{
+		_baseEnergy = Energy;
+		_baseScale = TextureScale;
+
+		// Unique offset so multiple lights don't sync
+		_offset = GD.Randf() * 1000f;
+	}
+
+	public override void _Process(double delta)
+	{
+		_time += (float)delta;
+
+		float wave = Mathf.Sin(_time * Speed + _offset);
+
+		Energy = _baseEnergy + wave * EnergyVariation;
+		TextureScale = _baseScale + wave * SizeVariation;
+	}
+}
